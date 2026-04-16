a36cdcc759a1933334ba0ee6c0bfd0ec92a8b9c5
cvast <hechenghao1998@foxmail.com>
2026-04-16 15:18:57 +0800

    feat(gateway): 配置网关端口并集成Nacos服务发现
    - 在application.yml中设置服务器端口为9090
    - 为simple-auth模块添加Nacos服务发现依赖
    - 为simple-auth模块添加SpringBoot Web依赖
    - 将gateway模块的starter从spring-boot-starter改为spring-boot-starter-web
    - 为auth服务创建完整的application.yml配置文件
    - 添加AuthApplication启动类并配置基本注解

---
+++ simple-auth/src/main/java/com/vast/auth/AuthApplication.java
@@ -0,0 +1,20 @@
+package com.vast.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright (C), 2020-2026, c-vast工作室
+ *
+ * @FileName: AuthApplication
+ * @Author: hechenghao1998@foxmail.com
+ * @Date: 2026/4/16 14:53
+ * @Description:
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class,args);
+    }
+}
